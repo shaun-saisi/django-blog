d435d337230827fac5116779f774ccf0cd660e24
anunda-shaun <saisishaun4@gmail.com>
2024-10-14 08:21:18 +0000

    deployed the basic blog ready for jenkins configurations

--- website/website/settings.py
+++ website/website/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-c7k(2^f%vzp*u1=w@2w+d6u$sz(l=^3c2_4b2gwn1#5b*c4ze1
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['159.223.173.109', 'localhost']
 
 
 # Application definition
@@ -125,6 +125,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
